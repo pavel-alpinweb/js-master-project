cfe2d07802d79be24da2af28c58468e753a343b1
pavel-alpinweb <pavel.alpinweb@yandex.ru>
2021-04-23 09:48:19 +0600

    resize - fix Vladilen way

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,41 +1,39 @@
-import {ExcelComponent} from '@core/ExcelComponent'
-import {createTable} from '@/components/table/table.template'
-import {$} from '@core/dom'
+import {ExcelComponent} from '@core/ExcelComponent';
+import {createTable} from '@/components/table/table.template';
+import {$} from '@core/dom';
 
 export class Table extends ExcelComponent {
   static className = 'excel__table'
 
   constructor($root) {
     super($root, {
-      listeners: ['mousedown']
-    })
+      listeners: ['mousedown'],
+    });
   }
 
   toHTML() {
-    return createTable(20)
+    return createTable(20);
   }
 
   onMousedown(event) {
     if (event.target.dataset.resize) {
-      const $resizer = $(event.target)
-      // const $parent = $resizer.$el.parentNode // bad!
-      // const $parent = $resizer.$el.closest('.column') // better but bad
-      const $parent = $resizer.closest('[data-type="resizable"]')
-      const coords = $parent.getCoords()
-      console.log()
+      const $resizer = $(event.target);
+      const $parent = $resizer.closest('[data-type="resizable"]');
+      const coords = $parent.getCoords();
+      console.log();
 
       document.onmousemove = e => {
-        console.log('mousemove')
-        const delta = e.pageX - coords.right
-        const value = coords.width + delta
-        $parent.$el.style.width = value + 'px'
+        console.log('mousemove');
+        const delta = e.pageX - coords.right;
+        const value = coords.width + delta;
+        $parent.$el.style.width = value + 'px';
         document.querySelectorAll(`[data-col="${$parent.data.col}"]`)
-            .forEach(el => el.style.width = value + 'px')
-      }
+            .forEach(el => el.style.width = value + 'px');
+      };
 
       document.onmouseup = () => {
-        document.onmousemove = null
-      }
+        document.onmousemove = null;
+      };
     }
   }
 }
--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -1,12 +1,12 @@
 const CODES = {
   A: 65,
-  Z: 90
-}
+  Z: 90,
+};
 
 function toCell(_, col) {
   return `
     <div class="cell" contenteditable data-col="${col}"></div>
-  `
+  `;
 }
 
 function toColumn(col, index) {
@@ -15,11 +15,11 @@ function toColumn(col, index) {
       ${col}
       <div class="col-resize" data-resize="col"></div>
     </div>
-  `
+  `;
 }
 
 function createRow(index, content) {
-  const resize = index ? '<div class="row-resize" data-resize="row"></div>' : ''
+  const resize = index ? '<div class="row-resize" data-resize="row"></div>' : '';
   return `
     <div class="row">
       <div class="row-info">
@@ -28,33 +28,33 @@ function createRow(index, content) {
       </div>
       <div class="row-data">${content}</div>
     </div>
-  `
+  `;
 }
 
 function toChar(_, index) {
-  return String.fromCharCode(CODES.A + index)
+  return String.fromCharCode(CODES.A + index);
 }
 
 export function createTable(rowsCount = 15) {
-  const colsCount = CODES.Z - CODES.A + 1 // Compute cols count
-  const rows = []
+  const colsCount = CODES.Z - CODES.A + 1; // Compute cols count
+  const rows = [];
 
   const cols = new Array(colsCount)
       .fill('')
       .map(toChar)
       .map(toColumn)
-      .join('')
+      .join('');
 
-  rows.push(createRow(null, cols))
+  rows.push(createRow(null, cols));
 
   for (let i = 0; i < rowsCount; i++) {
     const cells = new Array(colsCount)
         .fill('')
         .map(toCell)
-        .join('')
+        .join('');
 
-    rows.push(createRow(i + 1, cells))
+    rows.push(createRow(i + 1, cells));
   }
 
-  return rows.join('')
+  return rows.join('');
 }
